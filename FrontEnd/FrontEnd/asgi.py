762b8b03808e7ccffa16fe598fc29173637596f1
Gabriel Borges <gabriel.bgs00@gmail.com>
2020-08-26 19:01:05 -0300

    Revert "rearraging folder"
    This reverts commit b88b333df6d9e45a1f2167dcec982fffc7fc4e82.

--- FrontEnd/FrontEnd/asgi.py
+++ FrontEnd/FrontEnd/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SpotifyStats project.
+ASGI config for FrontEnd project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpotifyStats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FrontEnd.settings')
 
 application = get_asgi_application()
