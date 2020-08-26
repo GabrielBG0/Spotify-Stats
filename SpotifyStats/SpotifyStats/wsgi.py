b88b333df6d9e45a1f2167dcec982fffc7fc4e82
Gabriel Borges <gabriel.bgs00@gmail.com>
2020-08-26 18:55:32 -0300

    rearraging folder

--- SpotifyStats/SpotifyStats/wsgi.py
+++ SpotifyStats/SpotifyStats/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FrontEnd project.
+WSGI config for SpotifyStats project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FrontEnd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpotifyStats.settings')
 
 application = get_wsgi_application()
