2e81c97ef896a690591603ee83cd1ea36c7d6036
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-01-18 20:19:39 -0300

    metric means

--- BackEnd/PlaylistAnalizer.py
+++ BackEnd/PlaylistAnalizer.py
@@ -1,2 +1,26 @@
+from typing import Dict
 from Startup import SpotipyClient
 from decouple import config
+import numpy as np
+from PLalistTools import PlaylistDecomposer
+
+
+class PlaylistAnalizer:
+
+    def __init__(self, client):
+        if client is None:
+            raise Exception('No Spotipy client privided')
+
+        self.client = client
+
+    def metricStatistics(self, metrics):
+        tools = PlaylistDecomposer(self.client)
+        means = dict()
+        playlist = tools.getPlaylistTracks()
+
+        for metric in metrics:
+            sup = list()
+            for track in playlist:
+                sup.append(track[0][metric])
+            means[metric] = np.mean(sup)
+        return means
